752662fcc64a3e278950b6a95dda94686199b8fb
Chris Zembower <chris.zembower@nbcuni.com>
2021-02-14 12:54:07 -0500

    try renaming class

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,26 +4,25 @@
 #
 # @example
 #   include blob
-#class blob (
-#  String                    $account,
-#  String                    $blob_path,
-#  String                    $path       = $title,
-#  String                    $client_id  = $::client_id,
-#  String                    $mode       = '0644',
-#  Enum['present', 'absent'] $ensure     = present
-#) {
-#  blob::get { 'this':
-#    ensure    => $ensure,
-#    account   => $account,
-#    blob_path => $blob_path,
-#    client_id => $client_id,
-#    mode      => $mode
-#  }
-#
-#  file { 'this':
-#    path    => $path,
-#    mode    => $mode,
-#    require => Blob::Get['this']
-#  }
-#}
+class az_blob (
+  String                    $account,
+  String                    $blob_path,
+  String                    $path       = $title,
+  String                    $client_id  = $::client_id,
+  String                    $mode       = '0644',
+  Enum['present', 'absent'] $ensure     = present
+) {
+  blob::get { 'this':
+    ensure    => $ensure,
+    account   => $account,
+    blob_path => $blob_path,
+    client_id => $client_id,
+    mode      => $mode
+  }
 
+  file { 'this':
+    path    => $path,
+    mode    => $mode,
+    require => Blob::Get['this']
+  }
+}
