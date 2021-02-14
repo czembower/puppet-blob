b5ab50fa3079a745ef125843272e754695239fa7
Chris Zembower <chris.zembower@nbcuni.com>
2021-02-14 12:03:37 -0500

    add file resource

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,46 @@
-# Class: blob
-# This doesn't work because r10k ignores
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include blob
 class blob (
-  Enum['absent', 'present'] $ensure,
-  String                    $path,
   String                    $account,
-  String                    $client_id,
   String                    $blob_path,
-  String                    $owner,
-  String                    $mode
+  String                    $path       = $title,
+  String                    $client_id  = $::client_id,
+  String                    $mode       = '0644',
+  Enum['present', 'absent'] $ensure     = present
 ) {
-  contain blob::get
+  blob::get { 'this':
+    ensure    => $ensure,
+    account   => $account,
+    blob_path => $blob_path,
+    client_id => $client_id,
+    mode      => $mode
+  }
 
   file { 'this':
-    path  => $path,
-    owner => $owner,
-    mode  => $mode
+    path => $path,
+    mode => $mode
   }
 }
+
+#class blob (
+#  Enum['absent', 'present'] $ensure,
+#  String                    $path,
+#  String                    $account,
+#  String                    $client_id,
+#  String                    $blob_path,
+#  String                    $owner,
+#  String                    $mode
+#) {
+#  contain blob::get
+#
+#  file { 'this':
+#    path  => $path,
+#    owner => $owner,
+#    mode  => $mode
+#  }
+#}
+#
