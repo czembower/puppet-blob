d48460c3612a1d555a8e35f53de3d83e08428e44
Chris Zembower <chris.zembower@nbcuni.com>
2021-02-14 12:43:25 -0500

    debug

--- manifests/get.pp
+++ manifests/get.pp
@@ -1,15 +1,16 @@
 #This doesn't work because r10k ignores
-class blob::get (
-  Enum['absent', 'present'] $ensure    = 'present',
-  String                    $path      = blob::path,
-  String                    $account   = blob::account,
-  String                    $client_id = blob::client_id,
-  String                    $blob_path = blob::blob_path
-) {
-  blob_get { $path:
-    ensure    => $ensure,
-    account   => $account,
-    client_id => $client_id,
-    blob_path => $blob_path
-  }
-}
+#class blob::get (
+#  Enum['absent', 'present'] $ensure    = 'present',
+#  String                    $path      = blob::path,
+#  String                    $account   = blob::account,
+#  String                    $client_id = blob::client_id,
+#  String                    $blob_path = blob::blob_path
+#) {
+#  blob_get { $path:
+#    ensure    => $ensure,
+#    account   => $account,
+#    client_id => $client_id,
+#    blob_path => $blob_path
+#  }
+#}
+#
